96fb95233915a484d23785cf5b19e87a3e45622d
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-05-26 19:43:55 -0300

    Project created

---
+++ src/main/java/tech/backend/seuimovel/SeuimovelApplication.java
@@ -0,0 +1,13 @@
+package tech.backend.seuimovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeuimovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeuimovelApplication.class, args);
+	}
+
+}
